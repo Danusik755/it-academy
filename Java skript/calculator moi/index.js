2f0775a54914ddd508ec6da2438115f1577e5526
ilogic-7-a <daniilr96@gmail.com>
2024-12-12 17:57:16 +0300

    знятия с javascript

--- Java skript/calculator moi/index.js
+++ Java skript/calculator moi/index.js
@@ -18,8 +18,33 @@ function clear88() {
 }
 
 
+function app(value) {
+    //eval -  функция для javascript-кода
+    input.value =   input.value / value;
+}
 
-// let button1  = +firstNumber + +secondNumber
-// let raznost = firstNumber - secondNumber
-// let proizvedenie = firstNumber * secondNumber
-// let chasnoe = firstNumber / secondNumber
+function app1(value) {
+    //eval -  функция для javascript-кода
+    input.value =   input.value * value;
+}
+
+function compute1() {
+    let newValue = input.value.replace('*', '/');
+    newValue = input.value.replace('*', '/');
+    input.value = eval(newValue);
+    try {
+        input.value = eval(newValue);
+    } catch (e) {
+        input.value = 'Ошибка!';
+    }
+}
+function deleteKey(event) {
+let keys = ['1','2','3','4','5','6','7','8','9','0']
+    if ( keys.includes( event.key ) ==  false )
+        event.preventDefault()
+}
+
+input.addEventListener('keypress', deleteKey)
+
+
+
